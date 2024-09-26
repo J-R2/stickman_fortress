562d2f4dc0d150bcf0c808a2681968269234c5bc
Jordan <104177490+J-R2@users.noreply.github.com>
2024-09-26 15:01:31 -0500

    added player and gun scene

---
+++ scenes/player_scene/player.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var marker_gun_pivot: Marker2D = $MarkerGunPivot
+var direction := Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	marker_gun_pivot.look_at(get_global_mouse_position())
+	marker_gun_pivot.rotation = clampf(marker_gun_pivot.rotation, -PI/2, PI/2)
+	
+	
